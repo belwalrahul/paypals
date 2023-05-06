e031aeb9dd32fbb3d7f0c16ee1a2685c87fc4db6
Rahul Belwal <belwalrahul@gmail.com>
2023-05-06 14:59:10 -0700

    adding friend model

--- core/models.py
+++ core/models.py
@@ -2,15 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField()
-
-    def __str__(self):
-        return self.user.username
-
-    def save(self, *args, **kwargs):
-        super().save()
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    friends = models.ManyToManyField(User)
        
 class Group(models.Model):
     groupID = models.AutoField(primary_key=True)
