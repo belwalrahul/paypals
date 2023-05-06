76052bd5e35208525022512abc750d256847767c
Jait <jaitlimaye@gmail.com>
2023-05-06 14:07:47 -0700

    switched to postgres

--- paypals/settings.py
+++ paypals/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'paypals.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'paypals',
+        'USER': 'postgres',
+        'PASSWORD': 'Paypals465',
+        'HOST': '34.121.147.116',
+        'PORT': 5432
     }
 }
 
