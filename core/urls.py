8ba775cbd41ea2673f201c14f5febff440d1cca0
Rahul Belwal <rbelwal@csuchico.edu>
2023-05-09 21:39:04 -0700

    adding feature to delete a group

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('friends/', views.friends_list),
     path('charts/', views.charts),
     path('groups/<int:id>', views.grouppage),
+    path('delete_group/<int:pk>', views.delete_group),
     path('groups/<int:id>/add', views.add_grouptransaction),
     path('account_settings/', views.account_settings),
     path('remove_friend/<int:friend_id>/', views.remove_friend, name='remove_friend'),
